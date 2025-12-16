d66902fecfcfa8a9c6a1e6b90d23303e2f50d029
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-12-16 16:33:20 +0530

    Basic structure for center created

--- task-manager/src/components/Sidebar/Sidebar.jsx
+++ task-manager/src/components/Sidebar/Sidebar.jsx
@@ -13,7 +13,44 @@ function Sidebar() {
                 <input type="text" placeholder="Search" />
             </div>
         </div>
-        <div className="center"></div>
+        <div className="center">
+            <ul>
+                <p className="title">TASK</p>
+                <li>
+                    <span>Upcoming</span>
+                </li>
+                <li>
+                    <span>Today</span>
+                </li>
+                <li>
+                    <span>Calender</span>
+                </li>
+                <li>
+                    <span>Sticky Wall</span>
+                </li>
+            </ul>
+            <p className='title'>CATEGORY</p>
+            <ul>
+                <li>
+                    <div className="color"></div>
+                    <span>Personal</span>
+                </li>
+                <li>
+                    <div className="color"></div>
+                    <span>Work</span>
+                </li>
+                <li>
+                    <div className="color"></div>
+                    <span>Others</span>
+                </li>
+            </ul>
+            <p className='title'>TAGS</p>
+            <div className="tags">
+                <div className="tag add-tag">
+                    <input type="text" placeholder="New Add Tag" />
+                </div>
+            </div>
+        </div>
             
         <div className="bottom"></div>
     </div>
