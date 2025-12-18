b8edb671deae05abdd7c5efa4e4435af5d9de69c
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-12-18 15:16:45 +0530

    Title addedd

--- task-manager/src/components/Today/Today.jsx
+++ task-manager/src/components/Today/Today.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import './today.scss'
 
 function Today() {
   return (
-    <div>
+    <div className='today'>
+        <p className="title">Today</p>
       
     </div>
   )
