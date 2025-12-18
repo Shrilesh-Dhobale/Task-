dac110198e16d7e55636b5e6439ae15eb3eab3a9
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-12-18 15:24:57 +0530

    Today comp imported

--- task-manager/src/pages/Home/Home.jsx
+++ task-manager/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './home.scss'
 import Sidebar from '../../components/Sidebar/Sidebar.jsx'
+import Today from '../../components/Today/Today.jsx'
 
 function Home() {
   return (
@@ -12,7 +13,9 @@ function Home() {
           <span>12</span>
         </div>
 
-        <div className="home-top"></div>
+        <div className="home-top">
+          <Today />
+        </div>
         <div className="home-bottom">
           <div className="home-left"></div>
           <div className="home-right"></div>
