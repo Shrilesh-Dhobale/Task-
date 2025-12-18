67ad49498a46c5b9b0986c9637b5fcc973cb4beb
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-12-18 15:20:24 +0530

    Add icon imported

--- task-manager/src/components/Today/Today.jsx
+++ task-manager/src/components/Today/Today.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './today.scss'
+import {Add} from "@mui/icons-material";
 
 function Today() {
   return (
     <div className='today'>
         <p className="title">Today</p>
+        <ul>
+            <li style={{borderLeft:"1px solid #e6e6e6"}}>   
+                <Add/>
+            </li>
+        </ul>
       
     </div>
   )
